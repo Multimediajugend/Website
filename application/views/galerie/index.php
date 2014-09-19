c1bfe023080e88b8bcd400b1acb5441144c5186f
Martin Meyer <martinmeyer@outlook.com>
2014-09-19 22:07:53 +0200

    added branch for mvc
    publishing need followed changes:
    1) .htaccess:
    RewriteBase /Website/
    to
    RewriteBase /
    
    2) application/config/config.php
    define('URL', 'http://127.0.0.1/Website/');
    to
    define('URL', 'http://127.0.0.1/');

---
+++ application/views/galerie/index.php
@@ -0,0 +1,64 @@
+<div id="gallery">
+	<div class="pageTitle">Galerie</div>
+	<div class="galleryWrapper">
+		<div class="galleryGrid left">
+			<a href="subGallery.html">
+				<div class="gallerySingle">
+					<div class="galleryImage">
+						<img src="<?php echo URL; ?>public/img/gallery/preview1.jpg" /></div>
+					<div class="galleryExtra">
+						<div class="galleryTitle">Radtour 2014 #2</div>
+						<div class="galleryDate">27.07.214</div>
+						<div class="galleryDescription">Über den Elberadweg ging es von Dresden über Meißen nach Riesa.</div>
+					</div>
+				</div>
+			</a>
+			 <a href="subGallery.html">
+				<div class="gallerySingle">
+					<div class="galleryImage">
+						<img src="<?php echo URL; ?>public/img/gallery/preview3.jpg" /></div>
+					<div class="galleryExtra">
+						<div class="galleryTitle">Chemnitz zockt</div>
+						<div class="galleryDate">14.03.-16.03.2014</div>
+						<div class="galleryDescription">Zur Unterstützung in den Bereichen Technik und Catering auf der Lanparty "Chemnitz zockt".</div>
+					</div>
+				</div>
+			</a>
+			<a href="subGallery.html">
+				<div class="gallerySingle">
+					<div class="galleryImage">
+						<img src="<?php echo URL; ?>public/img/gallery/preview5.jpg" /></div>
+					<div class="galleryExtra">
+						<div class="galleryTitle">Radtour 2013</div>
+						<div class="galleryDate">29.09.2013</div>
+						<div class="galleryDescription">Von Dresden aus ging es über den Elberadweg bis Königstein.</div>
+					</div>
+				</div>
+			</a>
+		</div>
+		<div class="galleryGrid right">
+			<a href="subGallery.html">
+				<div class="gallerySingle">
+					<div class="galleryImage">
+						<img src="<?php echo URL; ?>public/img/gallery/preview2.jpg" /></div>
+					<div class="galleryExtra">
+						<div class="galleryTitle">Radtour 2014 #1</div>
+						<div class="galleryDate">25.05.2014</div>
+						<div class="galleryDescription">Die Radtour ging durch den Norden von Chemnitz entlang des Chemnitztalradwegs bis hin zu den Felsendomen Rabenstein.</div>
+					</div>
+				</div>
+			</a>
+			<a href="subGallery.html">
+				<div class="gallerySingle">
+					<div class="galleryImage">
+						<img src="<?php echo URL; ?>public/img/gallery/preview4.jpg" /></div>
+					<div class="galleryExtra">
+						<div class="galleryTitle">Clanwars X</div>
+						<div class="galleryDate">01.11.-03.11.2013</div>
+						<div class="galleryDescription">Die Jubiläumsausgabe der legendären Lanpary "Clanwars".</div>
+					</div>
+				</div>
+			</a>
+		</div>
+	</div>
+</div>
---
+++ application/views/home/index.php
@@ -0,0 +1,57 @@
+<div id="bigPictures">
+	<a href="<?php echo URL; ?>Kooperation">
+		<div class="bigPicture">
+			<img src="<?php echo URL; ?>public/img/start/bigPicture1.jpg" />
+			<div class="bigPicturePadding">
+				<div class="bigPictureTitle color-1">Kooperation</div>
+				<div class="bigPictureDescription">Hier ein ganz kurzer Teaser auf die Kategorie.</div>
+			</div>
+		</div>
+	</a>
+	<a href="index.html">
+		<div class="bigPicture">
+			<img src="<?php echo URL; ?>public/img/start/bigPicture2.jpg" />
+			<div class="bigPicturePadding">
+				<div class="bigPictureTitle color-2">Lanparty</div>
+				<div class="bigPictureDescription">Hier ein ganz kurzer Teaser auf die Kategorie.</div>
+			</div>
+		</div>
+	</a>
+	<a href="index.html">
+		<div class="bigPicture">
+			<img src="<?php echo URL; ?>public/img/start/bigPicture3.jpg" />
+			<div class="bigPicturePadding">
+				<div class="bigPictureTitle color-3">Musik</div>
+				<div class="bigPictureDescription">Hier ein ganz kurzer Teaser auf die Kategorie.</div>
+			</div>
+		</div>
+	</a>
+	<a href="index.html">
+		<div class="bigPicture last">
+			<img src="<?php echo URL; ?>public/img/start/bigPicture4.jpg" />
+			<div class="bigPicturePadding">
+				<div class="bigPictureTitle color-4">Sport</div>
+				<div class="bigPictureDescription">Hier ein ganz kurzer Teaser auf die Kategorie.</div>
+			</div>
+		</div>
+	</a>
+</div>
+<div id="news">
+	<div class="pageTitle">Aktuelles</div>
+	<div class="newsSingle">
+		<div class="newsHeadline">Ein Wochenende der Medien</div>
+		<div class="newsTeaser">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</div>
+		<div class="newsMeta">
+			<a href="news.php?id=1">weiterlesen</a><br />
+			(vom <span class="newsDate">01.01.1970</span>)
+		</div>
+	</div>
+	<div class="newsSingle">
+		<div class="newsHeadline">Ein Wochenende der Medien</div>
+		<div class="newsTeaser">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</div>
+		<div class="newsMeta">
+			<a href="news.php?id=1">weiterlesen</a><br />
+			(vom <span class="newsDate">01.01.1970</span>)
+		</div>
+	</div>
+</div>
