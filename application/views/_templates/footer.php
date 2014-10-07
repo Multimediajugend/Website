0b1d9ead02f4f25408d58afdb1486cc142877f2c
Tilmann Bach <tilli14@msn.com>
2014-10-07 18:46:33 +0200

    + added glyphicons

--- application/views/_templates/footer.php
+++ application/views/_templates/footer.php
@@ -1,13 +1,14 @@
 	<div id="footer">
 		<div class="footerbox">
 			Weiter Projekte:<br />
-			<a href="http://www.hsf-clanwars.de" target="_blank">www.hsf-clanwars.de</a><br />
-			<a href="http://www.damnsmalllan.de" target="_blank">www.damnsmalllan.de</a>
+			<a href="http://www.hsf-clanwars.de" target="_blank"><i class="fa fa-external-link fa-fw"></i> www.hsf-clanwars.de</a><br />
+			<a href="http://www.damnsmalllan.de" target="_blank"><i class="fa fa-external-link fa-fw"></i> www.damnsmalllan.de</a>
 		</div>
 		<div class="footerbox">
-			Fehler gefunden?<br />
-			<a href="https://github.com/Multimediajugend/Website/issues" target="_blank">Fehler melden!</a><br />
-			<a href="" onClick="$('#loginModal').trigger('openModal'); return false;">Admin</a>
+			Meta<br />
+			<a href="https://github.com/Multimediajugend/Website/issues" target="_blank"><i class="fa fa-warning fa-fw"></i> Fehler gefunden?</a><br />
+			<a href="<?php echo URL; ?>impressum" target="_blank"><i class="fa fa-info fa-fw"></i> Impressum</a><br />
+			<a href="" onClick="$('#loginModal').trigger('openModal'); return false;"><i class="fa fa-lock fa-fw"></i> Admin</a>
 		</div>
 		<div class="footerbox last">Copyright 2010-<?php echo date("Y"); ?> Design, Logo and Content by Multimediale Jugendarbeit Sachsen e.V.</div>
 	</div>
@@ -19,15 +20,15 @@
 		</div>
 		<form action="">
 			<div class="txt">
-				<label for="email">Email:</label>
-				<input id="email" type="text">
+				<label for="loginEmail">Email:</label>
+				<input id="loginEmail" type="text">
 			</div>
 			<div class="txt">
-				<label for="password">Password:</label>
-				<input id="password" type="password">
+				<label for="loginPassword">Password:</label>
+				<input id="loginPassword" type="password">
 			</div>
 			<div class="btn clearfix">
-				<a class="close" href="#">Login</a>
+				<a href="" onClick="login(); return false;">Login</a>
 				<a class="close cancel" href="#">Abbruch</a>
 			</div>
 		</form>
--- application/views/_templates/header.php
+++ application/views/_templates/header.php
@@ -7,6 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- css -->
     <link href="<?php echo URL; ?>public/css/style.css" rel="stylesheet">
+	<!-- font awesome -->
+    <link href="<?php echo URL; ?>public/css/font-awesome.min.css" rel="stylesheet">
     <!-- fonts-->
     <link href='http://fonts.googleapis.com/css?family=Nunito:400' rel='stylesheet' type='text/css'>
     <!-- jQuery -->
@@ -22,10 +24,10 @@
 	<div id="header">
             <a href="<?php echo URL; ?>"><img src="<?php echo URL; ?>public/img/mjslogo.svg" height="120" /></a>
             <span id="topNav">
-                <a <?php if(!isset($active)) echo 'class="active" '; ?>href="<?php echo URL; ?>">Start</a>
-                <a <?php if(isset($active) && $active=='member') echo 'class="active" '; ?>href="<?php echo URL; ?>mitglieder">Mitglieder</a>
-                <a <?php if(isset($active) && $active=='gallery') echo 'class="active" '; ?>href="<?php echo URL; ?>galerie">Galerie</a>
-                <a href="https://facebook.com/multimediajugend" target="_blank">Facebook</a>
+                <a <?php if(!isset($active)) echo 'class="active" '; ?>href="<?php echo URL; ?>"><i class="fa fa-home fa-fw"></i> Start</a>
+                <a <?php if(isset($active) && $active=='member') echo 'class="active" '; ?>href="<?php echo URL; ?>mitglieder"><i class="fa fa-group fa-fw"></i> Mitglieder</a>
+                <a <?php if(isset($active) && $active=='gallery') echo 'class="active" '; ?>href="<?php echo URL; ?>galerie"><i class="fa fa-photo fa-fw"></i> Galerie</a>
+                <a href="https://facebook.com/multimediajugend" target="_blank"><i class="fa fa-facebook fa-fw"></i> Facebook</a>
             </span>
 	</div>
 	<div id="motd">
