a7ef28ebb015a39ed0fc6c904236d7c8377ca62b
Tilmann Bach <tilli14@msn.com>
2014-10-10 16:34:04 +0200

    ~ modified footer layout

--- application/views/_templates/footer.php
+++ application/views/_templates/footer.php
@@ -4,10 +4,10 @@
 			<a href="http://www.hsf-clanwars.de" target="_blank"><i class="fa fa-external-link fa-fw"></i> www.hsf-clanwars.de</a><br />
 			<a href="http://www.damnsmalllan.de" target="_blank"><i class="fa fa-external-link fa-fw"></i> www.damnsmalllan.de</a>
 		</div>
-		<div class="footerbox">Copyright 2010-<?php echo date("Y"); ?> Design, Logo and Content by Multimediale Jugendarbeit Sachsen e.V.</div>
-		<div class="footerbox last">
+		<div class="footerbox middle">Copyright 2010-<?php echo date("Y"); ?> Design, Logo and Content by Multimediale Jugendarbeit Sachsen e.V.</div>
+		<div class="footerbox">
 			<a href="https://github.com/Multimediajugend/Website/issues" target="_blank"><i class="fa fa-warning fa-fw"></i> Fehler gefunden?</a><br />
-			<a href="<?php echo URL; ?>impressum" target="_blank"><i class="fa fa-info fa-fw"></i> Impressum</a><br />
+			<a href="<?php echo URL; ?>impressum"><i class="fa fa-info fa-fw"></i> Impressum</a><br />
 			<a href="" onClick="$('#loginModal').trigger('openModal'); return false;"><i class="fa fa-lock fa-fw"></i> Admin</a>
 		</div>
 	</div>
