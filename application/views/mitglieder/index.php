861cf03b21bbf46bbd7b4e757d8c1602c0ef64d4
Tilmann Bach <tilli14@msn.com>
2014-10-11 13:37:13 +0200

    made a button div a button

--- application/views/mitglieder/index.php
+++ application/views/mitglieder/index.php
@@ -68,7 +68,7 @@
 			</ul>
 			<div class="memberJoin">
 				<div class="memberJoinDescription">Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</div>
-				<div class="button">Mitglied werden</div>
+				<button>Mitglied werden</button>
 			</div>
 		</div>
 	</div>
