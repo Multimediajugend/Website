d3c32c99b2df3aec518624374858fe744c43519b
0hrpheus <martinmeyer@outlook.com>
2014-10-15 15:51:25 +0200

    + added example of news versioning

--- application/views/verein/index.php
+++ application/views/verein/index.php
@@ -59,15 +59,25 @@
                       if($news->published == null) {
                           echo '<i>noch nicht veröffentlicht</i>';
                       } else {
-                          echo '(vom <span class="newsDate">'.date("d.m.Y - H:i:s",strtotime($news->published)).'</span>)';
+                          echo '(vom <span class="newsDate">'.date("d.m.Y - H:i",strtotime($news->published)).'</span>)';
                       }
                   } ?>
 		</div>
         <div style="clear:left"></div>
         <div class="editSection">
-            <button class="newsEdit"><i class="fa fa-edit fa-fw"></i> bearbeiten</button>
-            <button><i class="fa fa-remove fa-fw"></i> löschen</button>
-            <button><i class="fa fa-eye-slash fa-fw"></i> anzeigen</button>
+            <button class="newsEdit"><i class="fa fa-edit fa-fw"></i> Bearbeiten</button>
+            <button><i class="fa fa-remove fa-fw"></i> Löschen</button><br />
+            Aktuelle Version: <i>keine veröffentlicht</i><br />
+            Version wählen: 
+            <select id="newsVersion">
+               <option value="5">5 - 15.10.14 - 10:20 (123)</option>
+               <option value="4">4 - 15.10.14 - 10:19</option>
+               <option value="3">3 - 15.10.14 - 10:17</option>
+               <option value="2">2 - 15.10.14 - 10:15</option>
+               <option value="1">1 - 15.10.14 - 10:09</option>
+             </select>
+            <button><i class="fa fa-eye fa-fw"></i> Anzeigen</button>
+            <button><i class="fa fa-upload fa-fw"></i> Version veröffentlichen</button>
         </div>
 	</div>
     <?php } ?>
